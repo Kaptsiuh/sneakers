b1a8db32db6c5e03d6d6eb4a08bf2fd199773496
Dima <dimaberry1@gmail.com>
2023-07-10 00:28:20 +0200

    feat: create header and content on main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" alt="Logo" />
+          <div>
+            <h3 className="text-uppercase">Sneakers</h3>
+            <p>Shop the best sneakers</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg" alt="Card" />
+            <span>100 $</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/user.svg" alt="User" />
+          </li>
+        </ul>
       </header>
+      <div className="content p-40">
+        <h1>All sneakers</h1>
+        <div className="card">
+          <img src="/img/sneakers/1.jpg" alt="Sneakers" />
+          <p></p>
+          <div>
+            <div>
+              <span>Price:</span>
+              <b>299 $</b>
+            </div>
+            <button>
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
