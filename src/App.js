bad77d9007fe2b1fec4b5bc65a566c57665ea249
Dima <dimaberry1@gmail.com>
2023-07-11 23:51:01 +0200

    feat: add drawer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="mb-20">Cart</h2>
+
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cartItemImg"
+            ></div>
+            <div className="mr-20 flex">
+              <p className="mb-5">Men Sneakers Nike Blazer Mid Suede</p>
+              <b>199 $</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" alt="Logo" />
           <div>
             <h3 className="text-uppercase">Sneakers</h3>
-            <p>Shop the best sneakers</p>
+            <p className="opacity-5">Shop the best sneakers</p>
           </div>
         </div>
         <ul className="d-flex">
@@ -20,18 +38,35 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1>All sneakers</h1>
-        <div className="card">
-          <img src="/img/sneakers/1.jpg" alt="Sneakers" />
-          <p></p>
-          <div>
-            <div>
-              <span>Price:</span>
-              <b>299 $</b>
+        <div className="d-flex align-center justify-between mb-40s">
+          <h1 className="mb-40">All sneakers</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Search..." />
+          </div>
+        </div>
+
+        <div className="d-flex">
+          <div className="card">
+            <div className="favorite">
+              <img src="/img/unliked.svg" alt="Unliked" />
             </div>
-            <button>
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/1.jpg"
+              alt="Sneakers"
+            />
+            <h5>Men Sneakers Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Price:</span>
+                <b>299 $</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
