f125483bcd82ba74c63f583a4cd21f837169f99a
Dima <dimaberry1@gmail.com>
2023-07-12 11:27:02 +0200

    feat: create drawer

--- src/App.js
+++ src/App.js
@@ -3,18 +3,50 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2 className="mb-20">Cart</h2>
+          <h2 className="d-flex justify-between mb-30">
+            Cart
+            <img
+              className="removeBtn cu-p"
+              src="/img/btn-remove.svg"
+              alt="Remove"
+            />
+          </h2>
 
-          <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-              className="cartItemImg"
-            ></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Men Sneakers Nike Blazer Mid Suede</p>
-              <b>199 $</b>
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Men Sneakers Nike Blazer Mid Suede</p>
+                <b>199 $</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
             </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+
+          <div className="cartTotalBlock">
+            <ul>
+              <li className="d-flex">
+                <span>Total:</span>
+                <div> </div>
+                <b>199 $</b>
+              </li>
+              <li className="d-flex">
+                <span>Tax 5%</span>
+                <div></div>
+                <b>10 $</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Make an order.
+              <img src="/img/arrow.svg" alt="Arrow" />
+            </button>
           </div>
         </div>
       </div>
