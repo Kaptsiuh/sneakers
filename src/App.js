66206e931fde09d7be28261b35e38d8c51850b75
Dima <dimaberry1@gmail.com>
2023-07-12 12:03:23 +0200

    feat: create components: Header, Drawer, Card

--- src/App.js
+++ src/App.js
@@ -1,74 +1,12 @@
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+
 function App() {
   return (
     <div className="wrapper clear">
-      <div className="overlay">
-        <div className="drawer">
-          <h2 className="d-flex justify-between mb-30">
-            Cart
-            <img
-              className="removeBtn cu-p"
-              src="/img/btn-remove.svg"
-              alt="Remove"
-            />
-          </h2>
-
-          <div className="items">
-            <div className="cartItem d-flex align-center mb-20">
-              <div
-                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-                className="cartItemImg"
-              ></div>
-              <div className="mr-20 flex">
-                <p className="mb-5">Men Sneakers Nike Blazer Mid Suede</p>
-                <b>199 $</b>
-              </div>
-              <img
-                className="removeBtn"
-                src="/img/btn-remove.svg"
-                alt="Remove"
-              />
-            </div>
-          </div>
-
-          <div className="cartTotalBlock">
-            <ul>
-              <li className="d-flex">
-                <span>Total:</span>
-                <div> </div>
-                <b>199 $</b>
-              </li>
-              <li className="d-flex">
-                <span>Tax 5%</span>
-                <div></div>
-                <b>10 $</b>
-              </li>
-            </ul>
-            <button className="greenButton">
-              Make an order.
-              <img src="/img/arrow.svg" alt="Arrow" />
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <header className="d-flex justify-between align-center p-40">
-        <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" alt="Logo" />
-          <div>
-            <h3 className="text-uppercase">Sneakers</h3>
-            <p className="opacity-5">Shop the best sneakers</p>
-          </div>
-        </div>
-        <ul className="d-flex">
-          <li className="mr-30">
-            <img width={18} height={18} src="/img/cart.svg" alt="Card" />
-            <span>100 $</span>
-          </li>
-          <li>
-            <img width={18} height={18} src="/img/user.svg" alt="User" />
-          </li>
-        </ul>
-      </header>
+      <Drawer />
+      <Header />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40s">
           <h1 className="mb-40">All sneakers</h1>
@@ -79,27 +17,7 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <div className="card">
-            <div className="favorite">
-              <img src="/img/unliked.svg" alt="Unliked" />
-            </div>
-            <img
-              width={133}
-              height={112}
-              src="/img/sneakers/1.jpg"
-              alt="Sneakers"
-            />
-            <h5>Men Sneakers Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Price:</span>
-                <b>299 $</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
+          <Card />
         </div>
       </div>
     </div>
